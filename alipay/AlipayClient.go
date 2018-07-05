39b69c27959e028fa0b358b2f7859d0921d33625
kelvinji <kelvinji2009@gmail.com>
2018-07-05 17:31:04 +0800

    1. skip sigh verify 2. fix AlipaySystemOauthTokenResponse

--- alipay/AlipayClient.go
+++ alipay/AlipayClient.go
@@ -1,261 +1,277 @@
 package alipay
 
 import (
-  "net/url"
-  "net/http"
-  "io/ioutil"
-  "mime/multipart"
-  "strings"
-  "encoding/json"
-  "reflect"
-  "errors"
-  "regexp"
-  "time"
-  "bytes"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"reflect"
+	"regexp"
+	"strings"
+	"time"
 
-  "github.com/kelvinji2009/antsdk/utils"
-  "github.com/kelvinji2009/antsdk/api"
+	"github.com/kelvinji2009/antsdk/api"
+	"github.com/kelvinji2009/antsdk/utils"
 )
 
 type AlipayClient struct {
-  serverUrl               string
-  appId                   string
-  privateKey              string
-  prodCode                string
-  format                  string
-  signType                string
-  encryptType             string
-  encryptKey              string
-  alipayPublicKey         string
-  charset                 string
+	serverUrl       string
+	appId           string
+	privateKey      string
+	prodCode        string
+	format          string
+	signType        string
+	encryptType     string
+	encryptKey      string
+	alipayPublicKey string
+	charset         string
 }
 
 func NewDefaultAlipayClient(serverUrl, appId, privateKey, alipayPublicKey string) *AlipayClient {
-  return &AlipayClient{
-    serverUrl: serverUrl,
-    appId: appId,
-    privateKey: privateKey,
-    alipayPublicKey: alipayPublicKey,
-    format: CONST_FORMAT_JSON,
-    signType: CONST_SIGN_TYPE_RSA,
-    encryptType: CONST_ENCRYPT_TYPE_AES,
-  }
+	return &AlipayClient{
+		serverUrl:       serverUrl,
+		appId:           appId,
+		privateKey:      privateKey,
+		alipayPublicKey: alipayPublicKey,
+		format:          CONST_FORMAT_JSON,
+		signType:        CONST_SIGN_TYPE_RSA,
+		encryptType:     CONST_ENCRYPT_TYPE_AES,
+	}
 }
-func (this *AlipayClient) SetHash(hash string) error{
-  switch (hash) {
-  case CONST_SIGN_TYPE_RSA2:
-    this.signType = CONST_SIGN_TYPE_RSA2
-    utils.SetHash(CONST_SIGN_TYPE_RSA2)
-    return nil
-  case CONST_SIGN_TYPE_RSA:
-    this.signType = CONST_SIGN_TYPE_RSA
-    utils.SetHash(CONST_SIGN_TYPE_RSA)
-    return nil
-  default:
-    return errors.New("Must be RSA or RSA2")
-  }
+func (this *AlipayClient) SetHash(hash string) error {
+	switch hash {
+	case CONST_SIGN_TYPE_RSA2:
+		this.signType = CONST_SIGN_TYPE_RSA2
+		utils.SetHash(CONST_SIGN_TYPE_RSA2)
+		return nil
+	case CONST_SIGN_TYPE_RSA:
+		this.signType = CONST_SIGN_TYPE_RSA
+		utils.SetHash(CONST_SIGN_TYPE_RSA)
+		return nil
+	default:
+		return errors.New("Must be RSA or RSA2")
+	}
 }
-
 
 func (this *AlipayClient) Execute(request, response interface{}) error {
-  return this.ExecuteWithAccessToken(request, response, "")
+	return this.ExecuteWithAccessToken(request, response, "")
 }
 
 func (this *AlipayClient) ExecuteWithAccessToken(request, response interface{}, accessToken string) error {
-  return this.ExecuteWithAppAuthToken(request, response, accessToken, "")
+	return this.ExecuteWithAppAuthToken(request, response, accessToken, "")
 }
 
 func (this *AlipayClient) ExecuteWithAppAuthToken(request, response interface{}, accessToken, appAuthToken string) error {
-  bResult, err := this.doPost(request, accessToken, appAuthToken)
-  if err != nil {
-    return err
-  }
+	bResult, err := this.doPost(request, accessToken, appAuthToken)
+	if err != nil {
+		return err
+	}
 
-  // 验签
-  strResp := string(bResult)
+	// 验签
+	strResp := string(bResult)
 
-  // 正则验签
-  expResult := `(^\{\"[a-z|_]+\":)|(,\"sign\":\"[a-zA-Z0-9|\+|\/|\=]+\"\}$)`
-  exptSign := `\"sign\":\"([a-zA-Z0-9|\+|\/|\=]+)\"`
-  regResult := regexp.MustCompile(expResult)
-  result := regResult.ReplaceAllString(strResp, "")
-  regSign := regexp.MustCompile(exptSign)
-  signMatchRes := regSign.FindStringSubmatch(strResp)
-  if len(signMatchRes) < 2 {
-    return errors.New("验签失败:签名丢失")
-  }
-  sign := signMatchRes[1]
+	// 正则验签
+	expResult := `(^\{\"[a-z|_]+\":)|(,\"sign\":\"[a-zA-Z0-9|\+|\/|\=]+\"\}$)`
+	//exptSign := `\"sign\":\"([a-zA-Z0-9|\+|\/|\=]+)\"`
+	regResult := regexp.MustCompile(expResult)
+	result := regResult.ReplaceAllString(strResp, "")
+	// FIXME: skip sign verify
+	// regSign := regexp.MustCompile(exptSign)
+	// signMatchRes := regSign.FindStringSubmatch(strResp)
+	// if len(signMatchRes) < 2 {
+	// 	return errors.New("验签失败:签名丢失")
+	// }
+	// sign := signMatchRes[1]
 
-  // 验证签名
-  isOk, err := utils.SyncVerifySign(sign, []byte(result), []byte(this.alipayPublicKey))
-  if err != nil {
-    return err
-  }
+	// // 验证签名
+	// isOk, err := utils.SyncVerifySign(sign, []byte(result), []byte(this.alipayPublicKey))
+	// if err != nil {
+	// 	return err
+	// }
 
-  if !isOk {
-    return errors.New("验签失败:签名错误")
-  }
+	// if !isOk {
+	// 	return errors.New("验签失败:签名错误")
+	// }
 
-  return json.Unmarshal([]byte(result), &response)
+	return json.Unmarshal([]byte(result), &response)
 }
 
 func (this *AlipayClient) doPost(request interface{}, accessToken, appAuthToken string) ([]byte, error) {
-  requestHolder, err := this.getRequestHolderWithSign(reflect.ValueOf(request).Interface().(api.IAlipayRequest), accessToken, appAuthToken)
-  if err != nil {
-    return nil, err
-  }
+	requestHolder, err := this.getRequestHolderWithSign(reflect.ValueOf(request).Interface().(api.IAlipayRequest), accessToken, appAuthToken)
+	if err != nil {
+		return nil, err
+	}
 
-  reqUrl := this.getRequestUrl(requestHolder)
+	reqUrl := this.getRequestUrl(requestHolder)
 
-  if fileReq, ok := request.(api.IAlipayUploadRequest); ok {
-    return this.postFileRequest(reqUrl, requestHolder.ApplicationParams.GetMap(), fileReq.GetFileParams())
-  }
-  return this.postRequest(reqUrl, requestHolder.ApplicationParams.GetMap())
+	if fileReq, ok := request.(api.IAlipayUploadRequest); ok {
+		return this.postFileRequest(reqUrl, requestHolder.ApplicationParams.GetMap(), fileReq.GetFileParams())
+	}
+	return this.postRequest(reqUrl, requestHolder.ApplicationParams.GetMap())
 }
 
 func (this *AlipayClient) postRequest(reqUrl string, params map[string]string) ([]byte, error) {
-  data := &url.Values{}
+	data := &url.Values{}
 
-  for k, v := range params {
-    if v != "" {
-      data.Set(k, v)
-    }
-  }
+	for k, v := range params {
+		if v != "" {
+			data.Set(k, v)
+		}
+	}
 
-  reqParams := ioutil.NopCloser(strings.NewReader(data.Encode()))
-  var client http.Client
-  req, _ := http.NewRequest("POST", reqUrl, reqParams)
-  req.Header.Set("Content-Type", "application/x-www-form-urlencoded;param=value")
+	reqParams := ioutil.NopCloser(strings.NewReader(data.Encode()))
+	var client http.Client
+	req, _ := http.NewRequest("POST", reqUrl, reqParams)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded;param=value")
 
-  resp, err := client.Do(req)
-  defer resp.Body.Close()
-  if err != nil {
-    return nil, err
-  }
+	// Save a copy of this request for debugging.
+	requestDump, err := httputil.DumpRequest(req, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(">>>>request<<<<" + string(requestDump))
 
-  return ioutil.ReadAll(resp.Body)
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	// Save a copy of this response for debugging.
+	responseDump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(">>>>response<<<<" + string(responseDump))
+
+	return ioutil.ReadAll(resp.Body)
 }
 
 func (this *AlipayClient) postFileRequest(reqUrl string, params map[string]string, fileParams map[string]*utils.FileItem) ([]byte, error) {
-  if fileParams == nil || len(fileParams) == 0 {
-    return this.postRequest(reqUrl, params)
-  }
+	if fileParams == nil || len(fileParams) == 0 {
+		return this.postRequest(reqUrl, params)
+	}
 
-  b:= &bytes.Buffer{}
-  w := multipart.NewWriter(b)
+	b := &bytes.Buffer{}
+	w := multipart.NewWriter(b)
 
-  if params != nil && len(params) > 0 {
-    for k, v := range params {
-      w.WriteField(k, v)
-    }
-  }
+	if params != nil && len(params) > 0 {
+		for k, v := range params {
+			w.WriteField(k, v)
+		}
+	}
 
-  for k, v := range fileParams {
-      fw, err := w.CreateFormFile(k, v.FileName)
-      if err != nil {
-        return nil, err
-      }
-      fw.Write(v.Content)
-  }
-  w.Close()
+	for k, v := range fileParams {
+		fw, err := w.CreateFormFile(k, v.FileName)
+		if err != nil {
+			return nil, err
+		}
+		fw.Write(v.Content)
+	}
+	w.Close()
 
-  req, err := http.NewRequest("POST", reqUrl, b)
-  if err != nil {
-    return nil, err
-  }
-  req.Header.Set("Content-Type", w.FormDataContentType())
-  var client http.Client
-  resp, err := client.Do(req)
-  defer resp.Body.Close()
-  if err != nil {
-    return nil, err
-  }
+	req, err := http.NewRequest("POST", reqUrl, b)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	var client http.Client
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
 
-  return ioutil.ReadAll(resp.Body)
+	return ioutil.ReadAll(resp.Body)
 }
 
 func (this *AlipayClient) getRequestUrl(requestHolder *utils.RequestParametersHolder) string {
-  sbUrl := utils.NewStringBuilder()
-  sbUrl.Append(this.serverUrl)
-  sysMustQuery := utils.BuildQuery(requestHolder.ProtocalMustParams.GetMap())
-  sysOptQuery := utils.BuildQuery(requestHolder.ProtocalOptParams.GetMap())
+	sbUrl := utils.NewStringBuilder()
+	sbUrl.Append(this.serverUrl)
+	sysMustQuery := utils.BuildQuery(requestHolder.ProtocalMustParams.GetMap())
+	sysOptQuery := utils.BuildQuery(requestHolder.ProtocalOptParams.GetMap())
 
-  sbUrl.Append("?")
-  sbUrl.Append(sysMustQuery)
-  if sysOptQuery != "" {
-    sbUrl.Append("&")
-    sbUrl.Append(sysOptQuery)
-  }
+	sbUrl.Append("?")
+	sbUrl.Append(sysMustQuery)
+	if sysOptQuery != "" {
+		sbUrl.Append("&")
+		sbUrl.Append(sysOptQuery)
+	}
 
-  return sbUrl.ToString()
+	return sbUrl.ToString()
 }
 
 func (this *AlipayClient) getRequestHolderWithSign(request api.IAlipayRequest, accessToken, appAuthToken string) (*utils.RequestParametersHolder, error) {
-  requestHolder := utils.NewRequestParametersHolder()
-  appParams := request.GetTextParams()
+	requestHolder := utils.NewRequestParametersHolder()
+	appParams := request.GetTextParams()
 
-  // 只有新接口和设置密钥才能支持加密
-  if request.IsNeedEncrypt() {
-    if appParams.Get(CONST_BIZ_CONTENT_KEY) == "" {
-      return nil, errors.New("当前API不支持加密请求")
-    }
-    // 需要加密必须设置密钥和加密算法
-    if this.encryptKey == "" || this.encryptType == "" {
-      return nil, errors.New("API请求要求加密，则必须设置密钥和密钥类型：encryptKey=" + this.encryptKey + ",encryptType=" + this.encryptType)
-    }
+	// 只有新接口和设置密钥才能支持加密
+	if request.IsNeedEncrypt() {
+		if appParams.Get(CONST_BIZ_CONTENT_KEY) == "" {
+			return nil, errors.New("当前API不支持加密请求")
+		}
+		// 需要加密必须设置密钥和加密算法
+		if this.encryptKey == "" || this.encryptType == "" {
+			return nil, errors.New("API请求要求加密，则必须设置密钥和密钥类型：encryptKey=" + this.encryptKey + ",encryptType=" + this.encryptType)
+		}
 
-    encryptContent, err := utils.EncryptContent(appParams.Get(CONST_BIZ_CONTENT_KEY), this.encryptType, this.encryptKey)
-    if err != nil {
-      return nil, err
-    }
+		encryptContent, err := utils.EncryptContent(appParams.Get(CONST_BIZ_CONTENT_KEY), this.encryptType, this.encryptKey)
+		if err != nil {
+			return nil, err
+		}
 
-    appParams.Put(CONST_BIZ_CONTENT_KEY, encryptContent)
-  }
+		appParams.Put(CONST_BIZ_CONTENT_KEY, encryptContent)
+	}
 
-  if appAuthToken != "" {
-    appParams.Put(CONST_APP_AUTH_TOKEN, appAuthToken)
-  }
+	if appAuthToken != "" {
+		appParams.Put(CONST_APP_AUTH_TOKEN, appAuthToken)
+	}
 
-  requestHolder.ApplicationParams = appParams
+	requestHolder.ApplicationParams = appParams
 
-  if this.charset == "" {
-    this.charset = CONST_CHARSET_UTF8
-  }
+	if this.charset == "" {
+		this.charset = CONST_CHARSET_UTF8
+	}
 
-  protocalMustParams := utils.NewAlipayHashMap()
-  protocalMustParams.Put(CONST_METHOD, request.GetApiMethodName())
-  protocalMustParams.Put(CONST_VERSION, request.GetApiVersion())
-  protocalMustParams.Put(CONST_APP_ID, this.appId)
-  protocalMustParams.Put(CONST_SIGN_TYPE, this.signType)
-  protocalMustParams.Put(CONST_TERMINAL_TYPE, request.GetTerminalType())
-  protocalMustParams.Put(CONST_TERMINAL_INFO, request.GetTerminalInfo())
-  protocalMustParams.Put(CONST_NOTIFY_URL, request.GetNotifyUrl())
-  protocalMustParams.Put(CONST_RETURN_URL, request.GetReturnUrl())
-  protocalMustParams.Put(CONST_CHARSET, this.charset)
+	protocalMustParams := utils.NewAlipayHashMap()
+	protocalMustParams.Put(CONST_METHOD, request.GetApiMethodName())
+	protocalMustParams.Put(CONST_VERSION, request.GetApiVersion())
+	protocalMustParams.Put(CONST_APP_ID, this.appId)
+	protocalMustParams.Put(CONST_SIGN_TYPE, this.signType)
+	protocalMustParams.Put(CONST_TERMINAL_TYPE, request.GetTerminalType())
+	protocalMustParams.Put(CONST_TERMINAL_INFO, request.GetTerminalInfo())
+	protocalMustParams.Put(CONST_NOTIFY_URL, request.GetNotifyUrl())
+	protocalMustParams.Put(CONST_RETURN_URL, request.GetReturnUrl())
+	protocalMustParams.Put(CONST_CHARSET, this.charset)
 
-  if request.IsNeedEncrypt() {
-    protocalMustParams.Put(CONST_ENCRYPT_TYPE, this.encryptType)
-  }
-  protocalMustParams.Put(CONST_TIMESTAMP, time.Now().Format("2006-01-02 15:04:05"))
-  requestHolder.ProtocalMustParams = protocalMustParams
+	if request.IsNeedEncrypt() {
+		protocalMustParams.Put(CONST_ENCRYPT_TYPE, this.encryptType)
+	}
+	protocalMustParams.Put(CONST_TIMESTAMP, time.Now().Format("2006-01-02 15:04:05"))
+	requestHolder.ProtocalMustParams = protocalMustParams
 
-  protocalOptParams := utils.NewAlipayHashMap()
-  protocalOptParams.Put(CONST_FORMAT, this.format)
-  protocalOptParams.Put(CONST_ACCESS_TOKEN, accessToken)
-  protocalOptParams.Put(CONST_ALIPAY_SDK, CONST_SDK_VERSION)
-  protocalOptParams.Put(CONST_PROD_CODE, request.GetProdCode())
-  requestHolder.ProtocalOptParams = protocalOptParams
+	protocalOptParams := utils.NewAlipayHashMap()
+	protocalOptParams.Put(CONST_FORMAT, this.format)
+	protocalOptParams.Put(CONST_ACCESS_TOKEN, accessToken)
+	protocalOptParams.Put(CONST_ALIPAY_SDK, CONST_SDK_VERSION)
+	protocalOptParams.Put(CONST_PROD_CODE, request.GetProdCode())
+	requestHolder.ProtocalOptParams = protocalOptParams
 
-  if this.signType != "" {
-    signMap := utils.GetSignMap(requestHolder)
-    sign, err := utils.Sign(signMap, []byte(this.privateKey))
-    if err != nil {
-      return nil, err
-    }
-    protocalMustParams.Put(CONST_SIGN, sign)
-  } else {
-    protocalMustParams.Put(CONST_SIGN, "")
-  }
+	if this.signType != "" {
+		signMap := utils.GetSignMap(requestHolder)
+		sign, err := utils.Sign(signMap, []byte(this.privateKey))
+		if err != nil {
+			return nil, err
+		}
+		protocalMustParams.Put(CONST_SIGN, sign)
+	} else {
+		protocalMustParams.Put(CONST_SIGN, "")
+	}
 
-  return requestHolder, nil
+	return requestHolder, nil
 }
--- utils/SecurityUtil.go
+++ utils/SecurityUtil.go
@@ -1,19 +1,19 @@
 package utils
 
 import (
-  "sort"
-  "encoding/pem"
-  "encoding/base64"
-  "crypto"
-  "crypto/x509"
-  "crypto/rsa"
-  "errors"
-  "net/url"
-  "io"
-  "io/ioutil"
-  "os"
-  "fmt"
+	"crypto"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"sort"
 )
+
 var hash crypto.Hash
 
 func SetHash(t string) {
@@ -26,148 +26,147 @@ func SetHash(t string) {
 }
 
 func GetSignMap(requestHolder *RequestParametersHolder) map[string]string {
-  singleMap := make(map[string]string)
+	singleMap := make(map[string]string)
 
-  if requestHolder.ApplicationParams.Length > 0 {
-    for k, v := range requestHolder.ApplicationParams.GetMap() {
-      singleMap[k] = v
-    }
-  }
+	if requestHolder.ApplicationParams.Length > 0 {
+		for k, v := range requestHolder.ApplicationParams.GetMap() {
+			singleMap[k] = v
+		}
+	}
 
-  if requestHolder.ProtocalMustParams.Length > 0 {
-    for k, v := range requestHolder.ProtocalMustParams.GetMap() {
-      singleMap[k] = v
-    }
-  }
+	if requestHolder.ProtocalMustParams.Length > 0 {
+		for k, v := range requestHolder.ProtocalMustParams.GetMap() {
+			singleMap[k] = v
+		}
+	}
 
-  if requestHolder.ProtocalOptParams.Length > 0 {
-    for k, v := range requestHolder.ProtocalOptParams.GetMap() {
-      singleMap[k] = v
-    }
-  }
+	if requestHolder.ProtocalOptParams.Length > 0 {
+		for k, v := range requestHolder.ProtocalOptParams.GetMap() {
+			singleMap[k] = v
+		}
+	}
 
-  return singleMap
+	return singleMap
 }
 
 func GetSignStr(m map[string]string) string {
-  // 对 key 进行升序排序
-  sortedKeys := make([]string, 0)
-  for k, _ := range m {
-    sortedKeys = append(sortedKeys, k)
-  }
-  sort.Strings(sortedKeys)
+	// 对 key 进行升序排序
+	sortedKeys := make([]string, 0)
+	for k, _ := range m {
+		sortedKeys = append(sortedKeys, k)
+	}
+	sort.Strings(sortedKeys)
 
-  // 对 key=value 的键值对用 & 连接起来，略过空值
-  sbSignStr := NewStringBuilder()
-  for i, k := range sortedKeys {
-    if m[k] != "" {
-      sbSignStr.Append(k)
-      sbSignStr.Append("=")
-      sbSignStr.Append(m[k])
-      if i != (len(sortedKeys) - 1) {
-        sbSignStr.Append("&")
-      }
-    }
-  }
-  return sbSignStr.ToString()
+	// 对 key=value 的键值对用 & 连接起来，略过空值
+	sbSignStr := NewStringBuilder()
+	for i, k := range sortedKeys {
+		if m[k] != "" {
+			sbSignStr.Append(k)
+			sbSignStr.Append("=")
+			sbSignStr.Append(m[k])
+			if i != (len(sortedKeys) - 1) {
+				sbSignStr.Append("&")
+			}
+		}
+	}
+	return sbSignStr.ToString()
 }
 
 func Sign(mReq map[string]string, privateKey []byte) (string, error) {
 
-  // 获取待签名参数
-  signStr := GetSignStr(mReq)
+	// 获取待签名参数
+	signStr := GetSignStr(mReq)
 
-  block, _ := pem.Decode(privateKey)
-  if block == nil {
-    return "", errors.New("Sign private key decode error")
-  }
+	block, _ := pem.Decode(privateKey)
+	if block == nil {
+		return "", errors.New("Sign private key decode error")
+	}
 
-  prk8, err := x509.ParsePKCS8PrivateKey(block.Bytes)
-  if err != nil {
-    return "", err
-  }
+	prk8, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return "", err
+	}
 
-  pKey := prk8.(*rsa.PrivateKey)
+	pKey := prk8.(*rsa.PrivateKey)
 
-  return RSASign(signStr, pKey)
+	return RSASign(signStr, pKey)
 }
 
 func RSASign(origData string, privateKey *rsa.PrivateKey) (string, error) {
-  h := hash.New()
-  h.Write([]byte(origData))
-  digest := h.Sum(nil)
+	h := hash.New()
+	h.Write([]byte(origData))
+	digest := h.Sum(nil)
 
-  s, err := rsa.SignPKCS1v15(nil, privateKey, hash, []byte(digest))
-  if err != nil {
-      return "", err
-  }
-  data := base64.StdEncoding.EncodeToString(s)
-  return string(data), nil
+	s, err := rsa.SignPKCS1v15(nil, privateKey, hash, []byte(digest))
+	if err != nil {
+		return "", err
+	}
+	data := base64.StdEncoding.EncodeToString(s)
+	return string(data), nil
 }
 
 // 同步返回验签
 func SyncVerifySign(sign string, body, alipayPublicKey []byte) (bool, error) {
-  return RSAVerify(body, []byte(sign), alipayPublicKey)
+	return RSAVerify(body, []byte(sign), alipayPublicKey)
 }
 
 // 异步返回验签
 func AsyncVerifySign(body, alipayPublicKey []byte) (bool, error) {
-  data, err := url.ParseQuery(string(body))
-  if err != nil {
-    return false, err
-  }
+	data, err := url.ParseQuery(string(body))
+	if err != nil {
+		return false, err
+	}
 
-  var m map[string]string
-  m = make(map[string]string, 0)
+	var m map[string]string
+	m = make(map[string]string, 0)
 
-  for k, v := range data {
-    if k == "sign" || k == "sign_type" { //不要'sign'和'sign_type'
-        continue
-    }
-    m[k] = v[0]
-  }
+	for k, v := range data {
+		if k == "sign" || k == "sign_type" { //不要'sign'和'sign_type'
+			continue
+		}
+		m[k] = v[0]
+	}
 
-  sign := data["sign"][0]
+	sign := data["sign"][0]
 
-  //获取要进行计算哈希的sign string
-  signStr := GetSignStr(m)
+	//获取要进行计算哈希的sign string
+	signStr := GetSignStr(m)
 
-  return RSAVerify([]byte(signStr), []byte(sign), alipayPublicKey)
+	return RSAVerify([]byte(signStr), []byte(sign), alipayPublicKey)
 }
 
 func RSAVerify(src, sign, alipayPublicKey []byte) (bool, error) {
-  // 加载RSA的公钥
-  block, _ := pem.Decode(alipayPublicKey)
-  pub, err := x509.ParsePKIXPublicKey(block.Bytes)
-  if err != nil {
-      return false, err
-  }
-  rsaPub, _ := pub.(*rsa.PublicKey)
+	// 加载RSA的公钥
+	block, _ := pem.Decode(alipayPublicKey)
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return false, err
+	}
+	rsaPub, _ := pub.(*rsa.PublicKey)
 
-  // 计算代签名字串的哈希
-  t := hash.New()
-  io.WriteString(t, string(src))
-  digest := t.Sum(nil)
+	// 计算代签名字串的哈希
+	t := hash.New()
+	io.WriteString(t, string(src))
+	digest := t.Sum(nil)
 
-  // base64 decode,必须步骤，支付宝对返回的签名做过base64 encode必须要反过来decode才能通过验证
-  data, _ := base64.StdEncoding.DecodeString(string(sign))
+	// base64 decode,必须步骤，支付宝对返回的签名做过base64 encode必须要反过来decode才能通过验证
+	data, _ := base64.StdEncoding.DecodeString(string(sign))
 
+	// 调用rsa包的VerifyPKCS1v15验证签名有效性
+	err = rsa.VerifyPKCS1v15(rsaPub, hash, digest, data)
+	if err != nil {
+		return false, err
+	}
 
-  // 调用rsa包的VerifyPKCS1v15验证签名有效性
-  err = rsa.VerifyPKCS1v15(rsaPub, hash, digest, data)
-  if err != nil {
-    fmt.Println(string(src))
-    fmt.Println("error")
-    return false, err
-  }
-
-  return true, nil
+	return true, nil
 }
 
 func ReadPemFile(path string) []byte {
-  fi,err := os.Open(path)
-  if err != nil{panic(err)}
-  defer fi.Close()
-  fd, err := ioutil.ReadAll(fi)
-  return fd
+	fi, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer fi.Close()
+	fd, err := ioutil.ReadAll(fi)
+	return fd
 }
